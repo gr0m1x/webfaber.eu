1be834dae3cc04ab70006a42c8ffbdb3e17b1f90
gr0m1x <gromixaka@gmail.com>
2018-12-04 01:28:56 +0200

    2я поправка

--- dev/js/build.js
+++ dev/js/build.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.project-slider').slick({
-      dots: true,
+    dots: true,
   })
 
-
+  // Slow scroll
   $("#site-nav").on("click","a", function (event) {
 
     event.preventDefault();
@@ -18,50 +18,55 @@ $(document).ready(function(){
    
   });
   
+  // hamburger
+  var hamburgerButton = $('.hamburger');
+  var siteNav = $('.site-nav_list');
 
-  var $hamburgerButton = $('.hamburger');
-  var $siteNav = $('.site-nav_list');
-
-  $hamburgerButton.click(toggleMenu);
+  hamburgerButton.click(toggleMenu);
 
   function toggleMenu() {
-    $hamburgerButton.toggleClass('is-active');
-    $siteNav.toggleClass('site-nav_list__visible');
+    hamburgerButton.toggleClass('is-active');
+    siteNav.toggleClass('site-nav_list__visible');
   }
-    
+
+  // site nav
+  var siteHeader = $('.site-header')
+  var headerOffsetTop = $('.site-nav_cont').offset().top;
+  
+  $(window).on('scroll', function(){
+    if (window.scrollY > headerOffsetTop) {
+      siteHeader.addClass('navigation-fixed');
+    } else {
+      siteHeader.removeClass('navigation-fixed');
+    }
+  })
+
+ 
+
 });
 
-var siteHeader = document.querySelector('.site-header')
-var headerOffsetTop = document.querySelector('.site-nav_cont').offsetTop;
-
-window.addEventListener('scroll', function(event) {
-    if (window.scrollY > headerOffsetTop) {
-        siteHeader.classList.add('navigation-fixed');
-    } else {
-        siteHeader.classList.remove('navigation-fixed');
-    }
-})
-
 function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
   
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 
 //   var technologiesSlider = document.querySelector('.tabccontainer');
 //   var windowsWight = document.documentElement.clientWidth;
