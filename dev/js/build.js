cf28334aa5daddadb3528777c76633f992f39a2e
gr0m1x <gromixaka@gmail.com>
2018-12-07 01:36:35 +0200

    nav list

--- dev/js/build.js
+++ dev/js/build.js
@@ -83,43 +83,26 @@ $(document).ready(function(){
 
 
 
+
 function openCity(evt, cityName) {
   
   // Declare all variables
   var i, tabcontent, tablinks;
-
+  
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
   }
-
+  
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
+ 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
-
-
-
-
-
-
-//   var technologiesSlider = document.querySelector('.tabccontainer');
-//   var windowsWight = document.documentElement.clientWidth;
-//   var mediaMiddle = 767;
-  
-//   function techSlide() {
-//       if (windowsWight < mediaMiddle) {
-//           technologiesSlider.classList.add('.technologies_slider');
-//       } else {
-//           siteHeader.classList.remove('.technologies_slider');
-//       }
-//   }
-//   techSlide();
